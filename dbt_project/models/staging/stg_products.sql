230f6b838827fa773a3729636a0d63b8c07d264a
lbluejay <floydeja@gmail.com>
2026-03-16 21:33:57 -0600

    fixing name

--- dbt_project/models/staging/stg_products.sql
+++ dbt_project/models/staging/stg_products.sql
@@ -5,7 +5,7 @@ with raw_products as (
 select
     nullif(trim(cast(product_id as string)), '') as product_id,
     nullif(trim(cast(product_category_name as string)), '') as product_category_name,
-    cast(nullif(trim(cast(product_name_length as string)), '') as float64) as product_name_length,
+    cast(nullif(trim(cast(product_name_lenght as string)), '') as float64) as product_name_length,
     cast(nullif(trim(cast(product_description_length as string)), '') as float64) as product_description_length,
     cast(nullif(trim(cast(product_photos_qty as string)), '') as float64) as product_photos_qty,
     cast(nullif(trim(cast(product_weight_g as string)), '') as float64) as product_weight_g,
